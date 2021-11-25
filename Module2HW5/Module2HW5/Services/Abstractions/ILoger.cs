da3b46714fa6f06616d0c668aca9160008978122
Andrew Kara <a.kara701@gmail.com>
2021-11-25 18:07:00 +0200

    Added Loger files

---
+++ Module2HW5/Module2HW5/Services/Abstractions/ILoger.cs
@@ -0,0 +1,13 @@
+﻿using Module2HW5.Helper;
+
+namespace Module2HW5.Services.Abstractions
+{
+    public interface ILoger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+        void Log(LogType logType, string message);
+        void End();
+    }
+}
---
+++ Module2HW5/Module2HW5/Services/Loger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Module2HW5.Helper;
+using Module2HW5.Services.Abstractions;
+
+namespace Module2HW5.Services
+{
+    public class Loger : ILoger
+    {
+        private readonly IFileService _fileService;
+
+        public Loger(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(LogType.Info, message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Log(LogType.Warning, message);
+        }
+
+        public void LogError(string message)
+        {
+            Log(LogType.Error, message);
+        }
+
+        public void Log(LogType logType, string message)
+        {
+            var log = $"{DateTime.UtcNow}: {logType.ToString()}: {message}";
+            _fileService.Log(log);
+
+            Console.WriteLine(log);
+        }
+
+        public void End()
+        {
+            _fileService.EndWriter();
+        }
+    }
+}
